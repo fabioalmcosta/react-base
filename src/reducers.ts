73185838e66492e0145d1be158a06892ae238fc5
Fabio Almeida Costa <40249761+fabioalmcosta@users.noreply.github.com>
2021-02-03 00:14:12 -0300

    Base App
    Added Ant D and created reducers auto identity

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,6 +1,26 @@
 import { createStore, combineReducers } from 'redux';
-import reducer from './modules/login/store/login.reducer';
 
-const store = createStore(reducer);
+let mappedReducers = {};
+
+const reducers = require.context(
+  // O caminho relativo da pasta
+  './modules',
+  // Se deve ou não olhar subpastas
+  true,
+  // Expressão regular para localizar nomes
+  /.reducer./,
+);
+
+export function setReducers(
+  requireComponent: __WebpackModuleApi.RequireContext,
+): void {
+  requireComponent.keys().forEach((fileName) => {
+    const routeImport = requireComponent(fileName).default;
+    mappedReducers = { ...mappedReducers, ...routeImport.component };
+  });
+}
+
+setReducers(reducers);
+const store = createStore(combineReducers(mappedReducers));
 
 export default store;
